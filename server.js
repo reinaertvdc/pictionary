14b944db363bd74b92330ec7785f8319c1c1d9ea
wout coenen <cwout.coenen@gmail.com>
2018-10-20 16:04:26 +0200

    room joining - room signaling - peer management (server side)

--- server.js
+++ server.js
@@ -83,7 +83,15 @@ wss.on('connection', (ws, req) => {
                 if (m.join !== undefined && typeof m.join.room === 'number' && typeof m.join.pass === 'string') {
                     if (rooms.rooms[m.join.room] !== undefined) {
                         if (rooms.rooms[m.join.room].pass === m.join.pass) {
+                            ws.roomNo = m.join.room;
+                            ws.peerNo = rooms.rooms[m.join.room].sockets.length;
+                            rooms.rooms[m.join.room].sockets.push(ws);
                             ws.send(JSON.stringify({join: {room: m.join.room, success: true}}));
+                            for (let i = 0; i < rooms.rooms[m.join.room].sockets.length - 1; i++) {
+                                if (rooms.rooms[m.join.room].sockets[i] !== undefined && rooms.rooms[m.join.room].sockets[i].readyState === 2) {
+                                    rooms.rooms[m.join.room].sockets[i].send(JSON.stringify({peers: rooms.rooms[m.join.room].sockets.length - 1}));
+                                }
+                            }
                         }
                         else if (m.join.pass === '') {
                             ws.send(JSON.stringify({join: {room: m.join.room, success: false, needPass: true}}));
@@ -96,9 +104,33 @@ wss.on('connection', (ws, req) => {
                         ws.send(JSON.stringify({join: {room: m.join.room, success: false}}));
                     }
                 }
+                if (typeof ws.roomNo === 'number' && rooms.rooms[ws.roomNo] !== undefined) {
+                    if (typeof m.peers === 'number') {
+                        console.log('b');
+                        ws.send(JSON.stringify({peers:rooms.rooms[ws.roomNo].sockets.length-1}));
+                    }
+                    if (m.signal !== undefined && typeof m.signal.peer === 'number' && typeof m.signal.signal === 'string') {
+                        console.log('c');
+                        let peer = m.signal.peer;
+                        if (peer >= ws.peerNo) peer++;
+                        let sock = rooms.rooms[ws.roomNo].sockets[peer];
+                        let from = ws.peerNo;
+                        console.log('from ' + from + ' to ' + peer);
+                        if (from !== peer) {
+                            if (from >= peer) from--;
+                            if (sock !== undefined) {
+                                sock.send(JSON.stringify({signal: {peer: from, signal: m.signal.signal}}));
+                            }
+                        }
+                    }
+                }
+                else {
+
+                }
                 //TODO: json data
             }
             catch (err) {
+                console.log(err);
                 let m = message;
                 console.log('string');
                 console.log(m);
@@ -111,6 +143,24 @@ wss.on('connection', (ws, req) => {
         }
     });
     ws.on('close', event => {
+        if (typeof ws.roomNo === 'number' && rooms.rooms[ws.roomNo] !== undefined) {
+            if (typeof ws.peerNo === 'number' && rooms.rooms[ws.roomNo].sockets[ws.peerNo] !== undefined) {
+                if (rooms.rooms[ws.roomNo].sockets[ws.peerNo].readyState <= 2)
+                    rooms.rooms[ws.roomNo].sockets[ws.peerNo].close();
+                rooms.rooms[ws.roomNo].sockets[ws.peerNo] = undefined;
+            }
+            let remove = true;
+            for (let i = 0; i < rooms.rooms[ws.roomNo].sockets.length; i++) {
+                if (rooms.rooms[ws.roomNo].sockets[i] !== undefined && rooms.rooms[ws.roomNo].sockets[i].readyState <= 2) {
+                    remove = false;
+                    break;
+                }
+            }
+            if (remove) {
+                rooms.removeRoom(ws.roomNo);
+                console.log('Removing room ' + ws.roomNo);
+            }
+        }
         console.log('WebSocket disconnected from: ' + addr);
     });
 });
